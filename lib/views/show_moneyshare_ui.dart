69ddac7df6ca893bc541ea54f3566a1f1c33366d
s6219410022 <s6219410022@sau.ac.th>
2022-02-11 15:54:28 +0700

    Code V.2 : Do xxxx....

--- lib/views/show_moneyshare_ui.dart
+++ lib/views/show_moneyshare_ui.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
 
 class ShowMoneyshareUI extends StatefulWidget {
-  const ShowMoneyshareUI({Key? key}) : super(key: key);
+  //สร้างตัวแปลรับค่าที่จะส่งมาจากอีกหน้าจอหน่ึง
+  double? money;
+  int? person;
+  double? tip;
+  double? moneyshare;
+
+  //ที่คอนสตรัส เอาตัวที่สร้างมารับค่าที่จะมาส่งจากอีกหน้าจอหนึ่ง
+  ShowMoneyshareUI({
+    Key? key,
+    this.money,
+    this.person,
+    this.tip,
+    this.moneyshare,
+  }) : super(key: key);
 
   @override
   _ShowMoneyshareUIState createState() => _ShowMoneyshareUIState();
@@ -10,6 +23,131 @@ class ShowMoneyshareUI extends StatefulWidget {
 class _ShowMoneyshareUIState extends State<ShowMoneyshareUI> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+       backgroundColor: Color(0xFFC1A0F1),
+      appBar: AppBar(
+        backgroundColor: Color(0xFF6B11EE),
+        title: Text(
+          'ผลการแชร์เงิน',
+        ),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 10.0,
+              ),
+              Image.asset(
+                'assets/images/dollar-icon.png',
+                height: 100.0,
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                'จำนวนเงินที่จะหาร',
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                widget.money.toString(),
+                style: TextStyle(
+                  color: Color(0xFF5A026A),
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              
+              ),
+              SizedBox( 
+                height: 10.0,
+
+              ),
+              Text( 
+                'บาท'
+              ),
+
+              Text(
+                'จำนวนคนที่จะหาร',
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                widget.person.toString(),
+                style: TextStyle(
+                  color: Color(0xFF5A026A),
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              
+              ),
+              SizedBox( 
+                height: 10.0,
+
+              ),
+              Text( 
+                'บาท'
+              ),
+              Text(
+                'จำนวนเงินทิป',
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                widget.tip.toString(),
+                style: TextStyle(
+                  color: Color(0xFF5A026A),
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              
+              ),
+              SizedBox( 
+                height: 10.0,
+
+              ),
+              Text( 
+                'บาท'
+              ),
+              Text(
+                'สรุปหารคนละ',
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width - 80.0,
+                height: 80.0,
+                color: Colors.cyan,
+                alignment: Alignment.center,
+                
+                child: Text(
+                  widget.moneyshare.toString(),
+                  style: TextStyle(
+                    color: Color(0xFFF80404),
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                
+                ),
+              ),
+              SizedBox( 
+                height: 10.0,
+
+              ),
+              Text( 
+                'บาท'
+              ),
+              
+            ],
+          
+          ),
+        ),
+      ),
+    );
   }
 }
